08762472d808be0ce62fc730e380e573e5ac50b4
Sean McGregor <smcgregor08@cmc.edu>
2012-08-16 10:43:19 -0700

    added Privly header to all requests

---
+++ background.js
@@ -0,0 +1,10 @@
+
+// Set the Privly version number as a header on all requests
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    details.requestHeaders.push({name:'X-Privly-Version', value:'0.0.7'});
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]);
+  
