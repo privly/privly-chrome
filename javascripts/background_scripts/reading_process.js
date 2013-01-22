8170151dda1f6a2fe9294db60437e65de6850acf
Sean McGregor <smcgregor08@cmc.edu>
2013-01-22 14:35:43 -0800

    broke background script up into its parts

---
+++ javascripts/background_scripts/reading_process.js
@@ -0,0 +1,50 @@
+//
+// browserAction Icon Interaction
+//
+chrome.browserAction.onClicked.addListener(function(tab) {
+  
+  // Set the text color to red
+  chrome.browserAction.setBadgeBackgroundColor({color: "#FF0000"});
+  
+  chrome.browserAction.getBadgeText({},
+    function(currentText) {
+      
+      //toggle the on/off state of the button
+      if (currentText === "off") {
+        chrome.browserAction.setBadgeText({text: ""});
+        chrome.browserAction.setTitle({title: "Turn Privly Viewing Off"});
+        
+        // If the badge text was previously set to off, we probably 
+        // need to inject the script.
+        chrome.tabs.captureVisibleTab(null, null, function(tab){
+          if (tab !== undefined) {
+            chrome.tabs.executeScript(tab.id, 
+              { file: "/javascripts/content_scripts/privly.js", 
+                allFrames: true });
+          }
+        })
+      } else {
+        chrome.browserAction.setBadgeText({text: "off"});
+        chrome.browserAction.setTitle({title: "Turn Privly Viewing On"});
+      }
+    });
+});
+
+//
+// Content Script Loading
+//
+// Loads content script if the badge text is not set to "off"
+//
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.browserAction.getBadgeText({},
+    function(currentText) {
+      if (currentText !== "off" && 
+          tab.url !== "chrome://newtab/" && 
+          changeInfo.status === "complete") {
+        chrome.tabs.executeScript(tabId, 
+          {file: "/javascripts/content_scripts/privly.js", 
+           allFrames: true,
+           runAt: "document_idle"});
+      }
+    });
+}); 
