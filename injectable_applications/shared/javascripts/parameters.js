be72fb07d4809cb136aa3b58984e7d00aadf2221
Sean McGregor <smcgregor08@cmc.edu>
2013-03-17 18:34:16 -0700

    removed old comment

--- injectable_applications/shared/javascripts/parameters.js
+++ injectable_applications/shared/javascripts/parameters.js
@@ -20,7 +20,6 @@ var privlyParameters = {
     var parameterHash = {};
     var parameterArray = parameterString.split("&");
     for (var i = 0; i < parameterArray.length; i++) {
-      //var currentParamterString = decodeURIComponent(parameterArray[i]);
       var pair = parameterArray[i].split("=");
       var key = decodeURIComponent(pair[0]);
       var value = decodeURIComponent(pair[1]);
