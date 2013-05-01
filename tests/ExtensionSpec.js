21b1eb373c4472f07e50de576476f0cad206c912
Daniel <daniel1555@gmail.com>
2013-04-30 19:00:54 -0700

    Added tests for first_run.js

--- tests/ExtensionSpec.js
+++ tests/ExtensionSpec.js
@@ -63,16 +63,185 @@ describe("Extension", function() {
             
             // Now that we have setup the test, it is time to check the
             modalButtonCallback(tab, testingCallback);
-            
           }
         );
       }
     );
   });
-  
 });
-
-
+describe ("First Run Suite", function() {
+    /*
+        Test the function that gets the running privly version
+    */
+    it("tests getPrivlyVersion", function() {
+        var output = getPrivlyVersion();
+        expect(output).toMatch(/\d+\.\d+\.\d+/);
+    });
+    /*
+        Test the function that gets the version in local storage
+    */
+    it("tests getStoredVersion", function() {
+        var output = getStoredVersion();
+        expect(output).toEqual(localStorage["version"]);
+    });
+    /*
+        Test the function that updates local storage with the running version
+    */
+    it("tests update_version", function() {
+        var existing = getStoredVersion();
+        var modified = existing+"99"
+        update_version(modified);
+        expect(getStoredVersion()).toEqual(modified);
+        update_version(existing);
+    });
+    /*
+        Test the function that launches the first_run.html page
+    */
+    it("tests firstrun", function(){
+      //Get the url of the first_run.html
+      var page = chrome.extension.getURL("pages/first_run.html");
+      var Window = 0;
+      //Look at all windows
+      chrome.windows.getAll({populate:true},function(window_list){
+        var count_before = 0;
+        var nope = 0;
+        for (var i = 0; i < window_list.length; i++){
+          //return all tabs in each window that match the first_run url
+          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
+            count_before+=1
+          });
+          waits(500);
+          //if on the last window
+         if (i === window_list.length-1){
+            //load new window
+            firstrun();
+            var count_after = 0;
+            //Look at all windows
+            chrome.windows.getAll({populate:true},function(win_list){
+              for(var i = 0; i < win_list.length; i++){
+                //return all tabs in each window that match the first_run url
+                chrome.tabs.query({windowId:parseInt(win_list[i].id,10),url:page},function(tab){
+                  count_after+=1;
+                });
+                waits(500);
+                if ( i === win_list.length-1){
+                    expect(count_after).toEqual(count_before+1);
+                }
+              }
+            });
+          }
+        }
+      });
+    });
+    /*
+        Test the function that launches the first_run.html page when not updated or new
+    */
+    it("tests run_firstrun when not new", function() {
+      var page = chrome.extension.getURL("pages/first_run.html");
+      var stored = getStoredVersion();
+      var running = getPrivlyVersion();
+      var tabs_before = 0;
+      chrome.windows.getAll({populate:true},function(win_list){
+        for(var i = 0; i < win_list.length; i++){
+          chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+            tabs_before+= tab.length;
+          });
+          waits(500);
+          if (i === win_list.length-1 && stored === running){
+            run_firstrun();
+            var tabs_after = 0;
+            chrome.windows.getAll({populate:true},function(wind_list){
+              for (var i = 0; i < wind_list.length; i++){
+                chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+                  tabs_after += tab.length;
+                });
+                waits(500);
+                if (i === wind_list.length-1){
+                  expect(tabs.length).toEqual(tabs_after.length);
+                }
+              }
+            });
+          } else {
+            expect(1).toEqual(1);
+          }
+        }
+      });
+    });
+    it("tests run_firstrun when new", function() {
+    /*
+        Test the function that launches the first_run.html page when new
+    */
+      var page = chrome.extension.getURL("pages/first_run.html");
+      var stored = getStoredVersion();
+      var running = getPrivlyVersion();
+      var tabs_before = 0;
+      chrome.windows.getAll({populate:true},function(win_list){
+        for(var i = 0; i < win_list.length; i++){
+          chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+            tabs_before+= tab.length;
+          });
+          waits(500);
+          if (i === win_list.length-1){
+            update_version(null);
+            run_firstrun();
+            update_version(stored);
+            var tabs_after = 0;
+            chrome.windows.getAll({populate:true},function(wind_list){
+              for (var i = 0; i < wind_list.length; i++){
+                chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+                  tabs_after += tab.length;
+                });
+                waits(500);
+                if (i === wind_list.length-1){
+                  expect(tabs.length).toEqual(tabs_after.length+1);
+                }
+              }
+            });
+          } else {
+            expect(1).toEqual(1);
+          }
+        }
+      });
+    });
+    /*
+        Test the function that launches the first_run.html page when updated
+    */
+    it("tests run_firstrun when updated", function() {
+      var page = chrome.extension.getURL("pages/first_run.html");
+      var stored = getStoredVersion();
+      var running = getPrivlyVersion();
+      var tabs_before = 0;
+      chrome.windows.getAll({populate:true},function(win_list){
+        for(var i = 0; i < win_list.length; i++){
+          chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+            tabs_before+= tab.length;
+          });
+          waits(500);
+          if (i === win_list.length-1){
+            //modify version in local storage
+            update_version(stored+"99");
+            run_firstrun();
+            //reset version to original
+            update_version(stored);
+            var tabs_after = 0;
+            chrome.windows.getAll({populate:true},function(wind_list){
+              for (var i = 0; i < wind_list.length; i++){
+                chrome.tabs.query({windowId:praseInt(win_list[i].id,10),url:page},function(tab){
+                  tabs_after += tab.length;
+                });
+                waits(500);
+                if (i === wind_list.length-1){
+                  expect(tabs.length).toEqual(tabs_after.length+1);
+                }
+              }
+            });
+          } else {
+            expect(1).toEqual(1);
+          }
+        }
+      });
+    });
+});
 
 (function() {
   var jasmineEnv = jasmine.getEnv();
