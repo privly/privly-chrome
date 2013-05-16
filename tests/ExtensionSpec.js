a92ecbb38e60fedc73bf68577e69dad04105aa34
Daniel <daniel1555@gmail.com>
2013-05-15 18:56:18 -0700

    Redirect test output to console

--- tests/ExtensionSpec.js
+++ tests/ExtensionSpec.js
@@ -244,13 +244,15 @@ describe ("First Run Suite", function() {
     });
 });
 
+
 (function() {
+  jasmine.getEnv().addReporter(new jasmine.ConsoleReporter());
   var jasmineEnv = jasmine.getEnv();
   jasmineEnv.updateInterval = 1000;
   
   var htmlReporter = new jasmine.HtmlReporter();
   
-  jasmineEnv.addReporter(htmlReporter);
+  //jasmineEnv.addReporter(htmlReporter);
   
   jasmineEnv.specFilter = function(spec) {
     return htmlReporter.specFilter(spec);
