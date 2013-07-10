66a9703a92907b9e92d217bdb7a6261f2f3cd3b5
Daniel <daniel1555@gmail.com>
2013-07-10 00:20:52 -0700

    Extensive cleanup and simplification of tests.

--- tests/ExtensionSpec.js
+++ tests/ExtensionSpec.js
@@ -80,345 +80,110 @@ describe("Extension", function() {
 });
 
 describe ("First Run Suite", function() {
-
   var page = chrome.extension.getURL("pages/first_run.html");
 
-  function close_window(){
+  function close_page(page){
     chrome.tabs.query({url:page},function(tabs){
-      if(tabs[0]){
-        chrome.windows.remove(tabs[0].windowId);
+      if(tabs.length > 1){
+        console.log("Bad news bears");
+      }
+      if(tabs[0]){ // if there is a first_run.html open
+        chrome.windows.remove(tabs[0].id); //close it
       }
     });
   }
-  function get_windows(callbacks){
-    var windows1 = null;
-    var flag1 = false;
-    runs(function(){
-      chrome.windows.getAll({populate:true},function(window_list){
-        windows1 = window_list;
-        flag1 = true;
-      });
-    });
-    waitsFor(function(){
-      return flag1;
-    },"Windows Should be defined",1000);
-    runs(function(){
-      console.log(windows1);
-      expect(windows1).not.toBe(null);
-      if (typeof callbacks[0] == "function"){
-        callbacks[0](windows1,callbacks.slice(1));
-      } else {
-        console.log("Failure2");
-      }
-    });
-  }
-
-  function check_window(a_window,callbacks){
-    var tab_count = 0;
-    var flag;
-    runs(function(){
-      flag = false;
-      //return all tabs in each window that match the first_run url
-      chrome.tabs.query({windowId:parseInt(a_window.id,10),url:page},
-        function(tab){
-          tab_count+=tab.length;
-         // tab_count+=1;
-          flag = true;
-        }
-      );
-    });
-    waitsFor(function(){
-      return flag;
-    },"Should sum tabs",1000);
-    runs(function(){
-      if (i === windows.length-1){
-        if (callbacks[0]){
-          if (typeof callbacks[0] == "function"){
-            callbacks[0](callbacks.slice(1));
-            return tab_count;
-          } else {
-            console.log("Failure1");
-          }
-        } else {
-          return tab_count;
-        }
-      }
-    });
-  }
-  
-  function count_of_tabs(windows,callbacks){
-    var flag,flag1 = false;
-    var tab_count = 0;
-    for (var i = 0; i < windows.length; i++){
-      tab_count += check_window(windows[i]);
-    }
-    console.log("Count of tabs is returning", tab_count);
-    return tab_count;
-    
-  }
-
-  function launch_window(callbacks){
-    firstrun();
-    if (typeof callbacks[0] == "function"){
-      callbacks[0](callbacks.slice(1));
-    } else {
-      console.log("Failure");
-    }
-  }
-  
-  function before_and_after(action){
-    var array_of_functions1 = [
-      count_of_tabs
-    ];
-    var array_of_functions2 = [
-      get_windows,
-      count_of_tabs
-    ];
-    console.log("Before");
-    var tabs_before = get_windows(array_of_functions1);
-    console.log("Middle");
-    console.log(tabs_before);
-    var tabs_after = action(array_of_functions2);
-    console.log("After");
-    console.log(tabs_after);
-    return (tabs_before, tabs_after);
-  }
-
-  /*
-   * Test the function that gets the running privly version
-   */
-  it("tests getPrivlyVersion", function() {
-      var output = getPrivlyVersion();
-      expect(output).toMatch(/\d+\.\d+\.\d+/);
+  beforeEach(function(){
+    close_page(page);
+  });
+  afterEach(function(){
+    close_page(page);
   });
 
   /*
-   * Test the function that gets the version in local storage
+   * Test the function that gets the running privly version.
    */
-  it("tests getStoredVersion", function() {
-      var output = getStoredVersion();
-      expect(output).toEqual(localStorage["version"]);
+  it("tests get_privly_version", function() {
+    var output = get_privly_version();
+    expect(output).toMatch(/\d+\.\d+\.\d+/);
   });
 
   /*
-   * Test the function that updates local storage with the running version
+   * Test the function that gets the version in local storage.
+   */
+  it("tests get_stored_version", function() {
+    var output = get_stored_version();
+    expect(output).toEqual(localStorage["version"]);
+  });
+
+  /*
+   * Test the function that updates local storage with the running version.
    */
   it("tests update_version", function() {
-      var existing = getStoredVersion();
-      var modified = existing + "99";
-      update_version(modified);
-      expect(getStoredVersion()).toEqual(modified);
-      update_version(existing);
+    var existing = get_stored_version();
+    var modified = existing + "99";
+    update_version(modified);
+    expect(get_stored_version()).toEqual(modified);
+    update_version(existing);
   });
 
+
   /*
-   * Test the function that launches the first_run.html page
+   * Test the function that launches the first_run.html page.
    */
   it("tests firstrun function", function(){
-    var array_of_functions1 = [
-      count_of_tabs
-    ];
-    var array_of_functions2 = [
-      get_windows,
-      count_of_tabs
-    ];
-    var tabs_before = null;
-    var tabs_after = null;
-    var flag1,flag2;
-    runs(function(){
-      flag1 = false;
-      tabs_before = get_windows(array_of_functions1);
-      while (typeof tabs_before === 'undefined'){
-        console.log("Tabs before is not undefined");
-        if (typeof tabs_before !== 'undefine') {
-          flag1 = true;
-        }
-      }
-    });
-    waitsFor(function(){
-      return flag1;
-    }, "Should sum tabs before",1000);
-    runs(function(){
-      flag2 = false;
-      tabs_after = launch_window(array_of_functions2);
-      while (typeof tabs_after === 'undefined'){
-        if (typeof tabs_after !== 'undefined') {
-          flag2 = true;
-        }
-      }
-    });
-    waitsFor(function(){
-      return flag2;
-    }, "Should sum tabs after",1000);
-    runs(function(){
-      console.log("In your base");
-      console.log(tabs_before,tabs_after);
-      expect(tabs_after).toEqual(tabs_before+1);
+    firstrun(); // launch first_run.html
+    chrome.tabs.query({url:page},function(tabs){
+      console.log(tabs.length);
+      expect(tabs.length).toEqual(1);
     });
   });
 
   /*
-   * Test the function that launches the first_run.html page when not updated or new
+   * Test the function that launches the first_run.html page when not new or
+   * updated.
    */
-  /*it("should not open firstrun html when not new or updated", function() {
-    var tabs_before = 0;
-    var tabs_after = 0;
-    var flag1,flag2;
-    runs(function(){
-      flag1 = false;
-      var stored = getStoredVersion();
-      var running = getPrivlyVersion();
-      chrome.windows.getAll({populate:true},function(window_list){
-        for(var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            tabs_before+= tab.length;
-          });
-          waits(500);
-          if (i === window_list.length-1 && stored === running){
-            flag1 = true;
-          }
-        }
-      });
+  it("should not open firstrun.html when not new or updated", function(){
+    var stored = get_stored_version();
+    var running = get_privly_version();
+    expect(stored).toEqual(running);
+
+    run_firstrun(); // execute firstrun launch logic 
+    chrome.tabs.query({url:page},function(tabs){
+      console.log(tabs.length);
+      expect(tabs.length).toEqual(0);
     });
-    waitsFor(function(){
-      return flag1;
-    }, "Should have counted all tabs before",2000);
-    runs(function(){
-      flag2 = false;
-      run_firstrun();
-      chrome.windows.getAll({populate:true},function(window_list){
-        for (var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            tabs_after += tab.length;
-          });
-          waits(500);
-          if (i === window_list.length-1){
-            close_window();
-            flag2 = true;
-          }
-        }
-      });
-    });
-    waitsFor(function(){
-      return flag2;
-    }, "Should have counted all tabs after",2000);
-    runs(function(){
-      expect(tabs_after).toEqual(tabs_before);
+  });
+  
+  /*
+   * Test the function that launches the first_run.html page when new.
+   */
+  it("should open firstrun.html when new", function(){
+    var stored = get_stored_version();
+    update_version(null);
+    run_firstrun(); // execute firstrun launch logic 
+    update_version(stored);
+    chrome.tabs.query({url:page},function(tabs){
+      console.log(tabs.length);
+      expect(tabs.length).toEqual(1);
     });
   });
 
   /*
-   * Test the function that launches the first_run.html page when new
+   * Test the function that launches the first_run.html page when updated.
    */
-  /*it("should open firstrun html when new", function() {
-    var stored = getStoredVersion();
-    var running = getPrivlyVersion();
-    var tabs_before = 0;
-    var tabs_after = 0;
-    var flag1,flag2;
-    runs(function(){
-      flag1 = false;
-      chrome.windows.getAll({populate:true},function(window_list){
-        for(var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            tabs_before+= tab.length;
-          });
-          waits(500);
-          if (i === window_list.length-1){
-            flag1 = true;
-          }
-        }
-      })
-    });
-    waitsFor(function(){
-      return flag1;
-    }, "Should have counted all tabs before",2000);
-    runs(function(){
-      flag2 = false;
-      update_version(null);
-      run_firstrun();
-      update_version(stored);
-      var tabs_after = 0;
-      chrome.windows.getAll({populate:true},function(window_list){
-        for (var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            tabs_after += tab.length;
-          });
-          waits(500);
-          if (i === window_list.length-1){
-            close_window();
-            flag2 = true;
-          }
-        }
-      });
-    });
-    waitsFor(function(){
-      return flag2;
-    }, "Should have counted all tabs after",2000);
-    runs(function(){
-      expect(tabs_after).toEqual(tabs_before+1);
+  it("should open firstrun.html when updated", function(){
+    var stored = get_stored_version();
+    var running = get_privly_version();
+    update_version(stored+"99");
+    var modified = get_stored_version();
+    expect(stored).not.toEqual(modified);
+    run_firstrun(); // execute firstrun launch logic 
+    update_version(stored); //restore starting version
+    chrome.tabs.query({url:page},function(tabs){
+      console.log(tabs.length);
+      expect(tabs.length).toEqual(1);
     });
   });
-
-  /*
-   * Test the function that launches the first_run.html page when updated
-   */
-  /*it("should open firstrun html when updated", function() {
-    var stored = getStoredVersion();
-    var running = getPrivlyVersion();
-    var tabs_before = 0;
-    var tabs_after = 0;
-    var flag1,flag2;
-    runs(function(){
-      flag1 = false;
-      chrome.windows.getAll({populate:true},function(window_list){
-        for(var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            //console.log(tabs_before,tab.length);
-            tabs_before+= tab.length;
-          });
-          waits(1000);
-          if (i === window_list.length-1){
-            //console.log(tabs_before);
-            flag1 = true;
-          }
-        }
-      });
-    });
-    waitsFor(function(){
-      return flag1;
-    }, "Should have counted all tabs before",2000);
-    runs(function(){
-      flag2 = false;
-      //modify version in local storage
-      update_version(stored+"99");
-      run_firstrun();
-      //reset version to original
-      var tabs_after = 0;
-      chrome.windows.getAll({populate:true},function(window_list){
-        for (var i = 0; i < window_list.length; i++){
-          chrome.tabs.query({windowId:parseInt(window_list[i].id,10),url:page},function(tab){
-            //console.log(tabs_after,tab.length);
-            tabs_after += tab.length;
-          });
-          waits(1000);
-          if (i === window_list.length-1){
-            //console.log(tabs_after);
-            update_version(stored);
-            close_window();
-            flag2 = true;
-          }
-        }
-      });
-    });
-    waitsFor(function(){
-      return flag2;
-    }, "Should have counted all tabs after",2000);
-    runs(function(){
-      console.log(tabs_after,tabs_before);
-      expect(tabs_after).toEqual(tabs_before+1);
-    });
-  });*/
 });
 
 
