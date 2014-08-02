729635ed98819c0a9a6433a800e8eeebfa099084
Sean McGregor <smcgregor08@cmc.edu>
2014-08-01 19:55:25 -0700

    namespaced posting_process.js

--- javascripts/background_scripts/posting_process.js
+++ javascripts/background_scripts/posting_process.js
@@ -15,183 +15,194 @@
  *    post_new_link.js, the URL
  */
 
-var messageSecret = null;
-
 /**
- * Handles right click on form event by opening posting window.
- *
- * @param {OnClickData} info Information on the context menu generating
- * this event.
- * @param {tab} sourceTab The tab that was clicked for the context menu
- * @param {string} postingApplicationName the name of the posting application.
- * for examples, see the creation of the context menus below. Current values
- * include PlainPost and ZeroBin
- *
+ * @namespace Functionality for posting new links to pages.
  */
-function postingHandler(info, sourceTab, postingApplicationName) {
+var postingProcess = {
   
-  // only open a new posting window
-  if (postingApplicationTabId === undefined) {
-    
-    var postingDomain = localStorage["posting_content_server_url"];
-    if ( postingDomain === undefined ) {
-      postingDomain = "https://privlyalpha.org";
-      localStorage["posting_content_server_url"] = postingDomain;
-    }
-    
-    var postingApplicationUrl = chrome.extension.getURL("privly-applications/" + 
-                                                         postingApplicationName + 
-                                                         "/new.html");
-    
-    if( info.selectionText !== undefined ) {
-      postingApplicationStartingValue = info.selectionText;
-    } else {
-      postingApplicationStartingValue = "";
-    }
-    
-    // Open a new window.
-    chrome.windows.create({url: postingApplicationUrl, focused: true,
-                           top: 0, left: 0,
-                           type: "normal"},
-      function(newWindow){
-      
-        //Get the window's tab
-        var tab = newWindow.tabs[0];
-      
-        //remember the posting tab id
-        postingApplicationTabId = tab.id;
-      
-        //remember the tab id where the post will be placed. The content script
-        //will remember which form element was clicked
-        postingResultTab = sourceTab;
-      
-        //tell the host page not to change the posting location on subsequent
-        //right click events
-        chrome.tabs.sendMessage(postingResultTab.id, {pendingPost: true});
+  /**
+   * The secret that should be included in messages to the application.
+   */
+  messageSecret: null,
+
+  /**
+   * Handles right click on form event by opening posting window.
+   *
+   * @param {OnClickData} info Information on the context menu generating
+   * this event.
+   * @param {tab} sourceTab The tab that was clicked for the context menu
+   * @param {string} postingApplicationName the name of the posting application.
+   * for examples, see the creation of the context menus below. Current values
+   * include PlainPost and ZeroBin
+   *
+   */
+  postingHandler: function(info, sourceTab, postingApplicationName) {
+
+    // only open a new posting window
+    if (postingProcess.postingApplicationTabId === undefined) {
+
+      var postingDomain = localStorage["posting_content_server_url"];
+      if ( postingDomain === undefined ) {
+        postingDomain = "https://privlyalpha.org";
+        localStorage["posting_content_server_url"] = postingDomain;
       }
-    );
-  } else {
-    var notification = new Notification("There is already a pending post",
-      {icon: "images/logo_48.png"});
-  }
-};
 
-/**
- * Handles the receipt of Privly URLs from the posting application 
- * for addition to the host page.
- *
- * @param {object} request The request object's JSON document. 
- * The request object should contain the privlyUrl.
- * @param {object} sender Information on the sending posting application
- * @param {function} sendResponse The callback function for replying to message
- *
- * @return {null} The function does not return anything, but it does call the
- * response function.
- */
-function receiveNewPrivlyUrl(request, sender, sendResponse) {
-  
-  if (request.handler === "privlyUrl" && postingResultTab !== undefined) {
-    
-    //Switches current tab to the page receiving the URL
-    chrome.tabs.update(postingResultTab.id, {selected: true});
-    
-    //sends URL to host page
-    chrome.tabs.sendMessage(postingResultTab.id, {privlyUrl: request.data, pendingPost: false});
-    
-    //close the posting application
-    chrome.tabs.remove(sender.tab.id);
-    postingApplicationTabId = undefined;
-    
-    //remove the record of where we are posting to
-    postingResultTab = undefined;
-  }
+      var postingApplicationUrl = chrome.extension.getURL("privly-applications/" +
+                                                           postingApplicationName +
+                                                           "/new.html");
+
+      if( info.selectionText !== undefined ) {
+        postingProcess.postingApplicationStartingValue = info.selectionText;
+      } else {
+        postingProcess.postingApplicationStartingValue = "";
+      }
+
+      // Open a new window.
+      chrome.windows.create({url: postingApplicationUrl, focused: true,
+                             top: 0, left: 0,
+                             type: "normal"},
+        function(newWindow){
+
+          //Get the window's tab
+          var tab = newWindow.tabs[0];
+
+          //remember the posting tab id
+          postingProcess.postingApplicationTabId = tab.id;
+
+          //remember the tab id where the post will be placed. The content script
+          //will remember which form element was clicked
+          postingProcess.postingResultTab = sourceTab;
+
+          //tell the host page not to change the posting location on subsequent
+          //right click events
+          chrome.tabs.sendMessage(postingProcess.postingResultTab.id,
+            {pendingPost: true});
+        }
+      );
+    } else {
+      var notification = new Notification("There is already a pending post",
+        {icon: "images/logo_48.png"});
+    }
+  },
+
+  /**
+   * Handles the receipt of Privly URLs from the posting application
+   * for addition to the host page.
+   *
+   * @param {object} request The request object's JSON document.
+   * The request object should contain the privlyUrl.
+   * @param {object} sender Information on the sending posting application
+   * @param {function} sendResponse The callback function for replying to message
+   *
+   * @return {null} The function does not return anything, but it does call the
+   * response function.
+   */
+  receiveNewPrivlyUrl: function(request, sender, sendResponse) {
+
+    if (request.handler === "privlyUrl" &&
+      postingProcess.postingResultTab !== undefined) {
+
+      //Switches current tab to the page receiving the URL
+      chrome.tabs.update(postingProcess.postingResultTab.id, {selected: true});
+
+      //sends URL to host page
+      chrome.tabs.sendMessage(postingProcess.postingResultTab.id,
+        {privlyUrl: request.data, pendingPost: false});
+
+      //close the posting application
+      chrome.tabs.remove(sender.tab.id);
+      postingProcess.postingApplicationTabId = undefined;
+
+      //remove the record of where we are posting to
+      postingProcess.postingResultTab = undefined;
+    }
+  },
+
+  /**
+   * Receives the secret message from the privly-application so
+   * it can send messages in the future with the secret token.
+   * Otherwise the applications will not trust the origin of the
+   * messages.
+   *
+   * @param {object} request The request object's JSON document.
+   * The request object should contain the privlyUrl.
+   * @param {object} sender Information on the sending posting application
+   * @param {function} sendResponse The callback function for replying to message
+   *
+   * @return {null} The function does not return anything, but it does call the
+   * response function.
+   */
+  initializeMessagePathway: function(request, sender, sendResponse) {
+
+    if (request.handler === "messageSecret" &&
+                 sender.tab.url.indexOf("chrome-extension://") === 0) {
+      postingProcess.messageSecret = request.data;
+      sendResponse({secret: postingProcess.messageSecret,
+                    handler: "messageSecret"});
+    } else if (request.handler === "initialContent" &&
+               sender.tab.id === postingProcess.postingApplicationTabId) {
+      sendResponse({secret: postingProcess.messageSecret, initialContent:
+                    postingProcess.postingApplicationStartingValue, handler: "initialContent"});
+    }
+  },
+
+  /**
+   * Send the privly-application the initial content, if there is any.
+   *
+   * @param {object} request The request object's JSON document.
+   * The request object should contain the privlyUrl.
+   * @param {object} sender Information on the sending posting application
+   * @param {function} sendResponse The callback function for replying to message
+   *
+   * @return {null} The function does not return anything, but it does call the
+   * response function.
+   */
+  sendInitialContent: function(request, sender, sendResponse) {
+
+    if (request.handler === "initialContent" &&
+               sender.tab.id === postingProcess.postingApplicationTabId) {
+      sendResponse({secret: postingProcess.messageSecret, initialContent:
+                    postingProcess.postingApplicationStartingValue, handler: "initialContent"});
+    } else if(request.handler === "initialContent") {
+      sendResponse({secret: postingProcess.messageSecret, initialContent: "",
+        handler: "initialContent"});
+    }
+  },
+
+  /**
+   * Handle closure of posting application tabs. If the posting application
+   * or host page closes, the state should reset. The posting form will close
+   * as well.
+   *
+   * @param {integer} tabId The ID of the tab removed.
+   * @param {removeInfo} removeInfo Information on the removal.
+   *
+   */
+  tabRemoved: function(tabId, removeInfo) {
+
+    if (postingProcess.postingResultTab === undefined ||
+        postingProcess.postingApplicationTabId === undefined) {
+      return;
+    }
+
+    // The tab generating the URL closed
+    if (tabId === postingProcess.postingApplicationTabId) {
+      chrome.tabs.sendMessage(postingProcess.postingResultTab.id, {pendingPost: false});
+    } else if (tabId === postingProcess.postingResultTab.id) {
+      // The tab receiving the URL Closed
+      chrome.tabs.remove(postingProcess.postingApplicationTabId);
+    }
+    postingProcess.postingResultTab = undefined;
+    postingProcess.postingApplicationTabId = undefined;
+    postingProcess.postingApplicationStartingValue = "";
+  },
+
+  // Remembers where the PrivlyUrl will be placed based on the context menu
+  postingResultTab: undefined,
+  postingApplicationTabId: undefined,
+  postingApplicationStartingValue: ""
+
 }
-
-/**
- * Receives the secret message from the privly-application so
- * it can send messages in the future with the secret token.
- * Otherwise the applications will not trust the origin of the
- * messages.
- *
- * @param {object} request The request object's JSON document. 
- * The request object should contain the privlyUrl.
- * @param {object} sender Information on the sending posting application
- * @param {function} sendResponse The callback function for replying to message
- *
- * @return {null} The function does not return anything, but it does call the
- * response function.
- */
-function initializeMessagePathway(request, sender, sendResponse) {
-  
-  if (request.handler === "messageSecret" && 
-               sender.tab.url.indexOf("chrome-extension://") === 0) {
-    messageSecret = request.data;
-    sendResponse({secret: messageSecret, 
-                  handler: "messageSecret"});
-  } else if (request.handler === "initialContent" && 
-             sender.tab.id === postingApplicationTabId) {
-    sendResponse({secret: messageSecret, initialContent: 
-                  postingApplicationStartingValue, handler: "initialContent"});
-  }
-}
-
-/**
- * Send the privly-application the initial content, if there is any.
- *
- * @param {object} request The request object's JSON document. 
- * The request object should contain the privlyUrl.
- * @param {object} sender Information on the sending posting application
- * @param {function} sendResponse The callback function for replying to message
- *
- * @return {null} The function does not return anything, but it does call the
- * response function.
- */
-function sendInitialContent(request, sender, sendResponse) {
-  
-  if (request.handler === "initialContent" && 
-             sender.tab.id === postingApplicationTabId) {
-    sendResponse({secret: messageSecret, initialContent: 
-                  postingApplicationStartingValue, handler: "initialContent"});
-  } else if(request.handler === "initialContent") {
-    sendResponse({secret: messageSecret, initialContent: "",
-      handler: "initialContent"});
-  }
-}
-
-/** 
- * Handle closure of posting application tabs. If the posting application 
- * or host page closes, the state should reset. The posting form will close 
- * as well.
- *
- * @param {integer} tabId The ID of the tab removed.
- * @param {removeInfo} removeInfo Information on the removal.
- *
- */
-function tabRemoved(tabId, removeInfo) {
-
-  if (postingResultTab === undefined || postingApplicationTabId === undefined) {
-    return;
-  }
-
-  // The tab generating the URL closed
-  if (tabId === postingApplicationTabId) {
-    chrome.tabs.sendMessage(postingResultTab.id, {pendingPost: false});
-    postingResultTab = undefined;
-    postingApplicationTabId = undefined;
-    postingApplicationStartingValue = "";
-  } else if (tabId === postingResultTab.id) {
-    // The tab receiving the URL Closed
-    chrome.tabs.remove(postingApplicationTabId);
-    postingResultTab = undefined;
-    postingApplicationTabId = undefined;
-    postingApplicationStartingValue = "";
-  }
-}
-
-// Remembers where the PrivlyUrl will be placed based on the context menu
-var postingResultTab = undefined;
-var postingApplicationTabId = undefined;
-var postingApplicationStartingValue = "";
 
 // Informs the user that they must have a developer account to post new content
 chrome.contextMenus.create({
@@ -205,7 +216,7 @@ chrome.contextMenus.create({
     "title": "Post with ZeroBin",
     "contexts": ["editable"],
     "onclick" : function(info, tab) {
-        postingHandler(info, tab, "ZeroBin");
+        postingProcess.postingHandler(info, tab, "ZeroBin");
     }
   });
   
@@ -214,7 +225,7 @@ chrome.contextMenus.create({
     "title": "Post with PlainPost",
     "contexts": ["editable"],
     "onclick" : function(info, tab) {
-        postingHandler(info, tab, "PlainPost");
+        postingProcess.postingHandler(info, tab, "PlainPost");
     }
   });
   
@@ -223,14 +234,14 @@ chrome.contextMenus.create({
     "title": "Post Existing Content",
     "contexts": ["editable"],
     "onclick" : function(info, tab) {
-        postingHandler(info, tab, "Index");
+        postingProcess.postingHandler(info, tab, "Index");
     }
   });
 
 // Initialize message listeners
-chrome.extension.onMessage.addListener(initializeMessagePathway);
-chrome.extension.onMessage.addListener(receiveNewPrivlyUrl);
-chrome.extension.onMessage.addListener(sendInitialContent);
+chrome.extension.onMessage.addListener(postingProcess.initializeMessagePathway);
+chrome.extension.onMessage.addListener(postingProcess.receiveNewPrivlyUrl);
+chrome.extension.onMessage.addListener(postingProcess.sendInitialContent);
 
 // Handle the request sent from post_new_link.js when clicking the Privly button
 chrome.runtime.onMessage.addListener(
@@ -253,4 +264,4 @@ chrome.runtime.onMessage.addListener(
   });
 
 // Handle closure of posting application tabs
-chrome.tabs.onRemoved.addListener(tabRemoved);
+chrome.tabs.onRemoved.addListener(postingProcess.tabRemoved);
